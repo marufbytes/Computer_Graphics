1f6f57a96fde5fd8c88d0b9e6797a0954366aeb1
marufbytes <marufahammed215@gmail.com>
2026-01-07 04:03:27 +0600

    push

---
+++ Basic/Cure_Rotation/main.cpp
@@ -0,0 +1,166 @@
+#include <GL/glut.h>
+
+float angle = 0.0f;
+
+void drawCube() {
+
+    glBegin(GL_QUADS);
+
+    // Front face (Red)
+
+    glColor3f(1.0, 0.0, 0.0);
+
+    glVertex3f(-1.0, -1.0,  1.0);
+
+    glVertex3f( 1.0, -1.0,  1.0);
+
+    glVertex3f( 1.0,  1.0,  1.0);
+
+    glVertex3f(-1.0,  1.0,  1.0);
+
+    // Back face (Green)
+
+    glColor3f(0.0, 1.0, 0.0);
+
+    glVertex3f(-1.0, -1.0, -1.0);
+
+    glVertex3f(-1.0,  1.0, -1.0);
+
+    glVertex3f( 1.0,  1.0, -1.0);
+
+    glVertex3f( 1.0, -1.0, -1.0);
+
+    // Top face (Blue)
+
+    glColor3f(0.0, 0.0, 1.0);
+
+    glVertex3f(-1.0,  1.0, -1.0);
+
+    glVertex3f(-1.0,  1.0,  1.0);
+
+    glVertex3f( 1.0,  1.0,  1.0);
+
+    glVertex3f( 1.0,  1.0, -1.0);
+
+    // Bottom face (Yellow)
+
+    glColor3f(1.0, 1.0, 0.0);
+
+    glVertex3f(-1.0, -1.0, -1.0);
+
+    glVertex3f( 1.0, -1.0, -1.0);
+
+    glVertex3f( 1.0, -1.0,  1.0);
+
+    glVertex3f(-1.0, -1.0,  1.0);
+
+    // Right face (Cyan)
+
+    glColor3f(0.0, 1.0, 1.0);
+
+    glVertex3f( 1.0, -1.0, -1.0);
+
+    glVertex3f( 1.0,  1.0, -1.0);
+
+    glVertex3f( 1.0,  1.0,  1.0);
+
+    glVertex3f( 1.0, -1.0,  1.0);
+
+    // Left face (Magenta)
+
+    glColor3f(1.0, 0.0, 1.0);
+
+    glVertex3f(-1.0, -1.0, -1.0);
+
+    glVertex3f(-1.0, -1.0,  1.0);
+
+    glVertex3f(-1.0,  1.0,  1.0);
+
+    glVertex3f(-1.0,  1.0, -1.0);
+
+    glEnd();
+
+}
+
+// Display function
+
+void display() {
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glLoadIdentity();
+
+    glTranslatef(0.0f, 0.0f, -6.0f); // Move cube into screen
+
+    glRotatef(angle, 1.0f, 1.0f, 0.0f); // Rotate cube
+
+    drawCube();
+
+    glutSwapBuffers();
+
+}
+
+void timer(int) {
+
+    angle += 1.0f;
+
+    if (angle > 360) angle -= 360;
+
+    glutPostRedisplay();
+
+    glutTimerFunc(10, timer, 0);
+
+}
+
+// Initialization
+
+void init() {
+
+    glEnable(GL_DEPTH_TEST); // Enable depth
+
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+
+}
+
+// Reshape function
+
+void reshape(int w, int h) {
+
+    glViewport(0, 0, w, h);
+
+    glMatrixMode(GL_PROJECTION);
+
+    glLoadIdentity();
+
+    gluPerspective(45.0, (float)w / h, 1.0, 100.0);
+
+    glMatrixMode(GL_MODELVIEW);
+
+}
+
+// Main function
+
+int main(int argc, char** argv) {
+
+    glutInit(&argc, argv);
+
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+
+    glutInitWindowSize(600, 600);
+
+    glutCreateWindow("3D Cube - OpenGL");
+
+    init();
+
+    glutDisplayFunc(display);
+
+    glutReshapeFunc(reshape);
+
+    glutTimerFunc(0, timer, 0);
+
+    glutMainLoop();
+
+    return 0;
+
+}
+
---
+++ Basic/Scaling/main.cpp
@@ -0,0 +1,50 @@
+#include <GL/glut.h>
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Original square (Red)
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glLoadIdentity();
+    glBegin(GL_QUADS);
+        glVertex2f(-0.4f, -0.4f);
+        glVertex2f( 0.4f, -0.4f);
+        glVertex2f( 0.4f,  0.4f);
+        glVertex2f(-0.4f,  0.4f);
+    glEnd();
+
+    // Scaled square (Blue)
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glLoadIdentity();
+    glScalef(1.5f, 0.5f, 0.0f);   // Scaling factors
+    glBegin(GL_QUADS);
+        glVertex2f(-0.4f, -0.4f);
+        glVertex2f( 0.4f, -0.4f);
+        glVertex2f( 0.4f,  0.4f);
+        glVertex2f(-0.4f,  0.4f);
+    glEnd();
+
+    glFlush();
+}
+
+// Initialization function
+void init() {
+    glClearColor(1.0, 1.0, 1.0, 1.0); // White background
+    glMatrixMode(GL_MODELVIEW);
+}
+
+// Main function
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(600, 600);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Scaling Transformation Visualization");
+
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
+
+    return 0;
+}
