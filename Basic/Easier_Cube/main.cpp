78ab42e1c61b9cf9a1fcc09ab707d88e34dd7f90
marufbytes <marufahammed215@gmail.com>
2026-01-07 06:22:05 +0600

    glut

--- Basic/Easier_Cube/main.cpp
+++ Basic/Easier_Cube/main.cpp
@@ -1,17 +1,18 @@
-#include<gl/GLUT.h>
+#include <GL/glut.h>
 
-float angle = 0;
+float angle = 0.0f;
 
 void display()
 {
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-    glTranslatef(0,0,-5);
-    glRotatef(angle,1,1,0);
+    glTranslatef(0, 0, -5);     // Move cube back
+    glRotatef(angle, 1, 1, 0);  // Rotate cube
 
     glBegin(GL_QUADS);
 
+    // Front face (Red)
     glColor3f(1, 0, 0);
     glVertex3f(-1,-1, 1);
     glVertex3f( 1,-1, 1);
@@ -55,43 +56,41 @@ void display()
 
     glEnd();
     glutSwapBuffers();
-
 }
 
 void timer(int)
 {
-    angle +=1;
+    angle += 1.0f;              // Increase rotation angle
     glutPostRedisplay();
-    glutTimerFunc(16,timer,0);
+    glutTimerFunc(16, timer, 0);
 }
 
 void init()
 {
     glEnable(GL_DEPTH_TEST);
-    glClearColor(0,0,0,1);
-
+    glClearColor(0, 0, 0, 1);
 }
 
 void reshape(int w, int h)
 {
- glViewport(0,0,w,h);
- glMatrixMode(GL_PROJECTION);
- glLoadIdentity();
- gluPerspective(45,(float)w/h,1,100);
- glMatrixMode(GL_MODELVIEW);
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45, (float)w / h, 1, 100);
+    glMatrixMode(GL_MODELVIEW);
 }
 
-int main(int a, char **b)
+int main(int argc, char** argv)
 {
-    glutInit(&a,b);
-    glutInitDisplayMode(GL_DOUBLE|GL_RGB|GL_DEPTH);
-    glutInitWindowSize(500,500);
-    glutCreateWindow("DDD");
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Rotating Cube - 6 Colors");
 
     init();
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
-    glutTimerFunc(0,timer,0);
+    glutTimerFunc(0, timer, 0);
 
     glutMainLoop();
     return 0;
